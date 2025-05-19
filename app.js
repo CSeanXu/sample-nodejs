c06118e6c833df81a8ca50fb27d768dc3b7506c0
SeanXu <xuqingjian@sjtu.edu.cn>
2025-05-19 20:22:25 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const koffi = require('koffi')
+const path = require('path')
+const os = require('os')
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+// Determine the correct library extension based on OS
+const getLibExtension = () => {
+  const platform = os.platform();
+  switch (platform) {
+    case 'darwin':
+      return '.dylib';
+    case 'linux':
+      return '.so';
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+};
+
+// Load the appropriate library
+const libPath = path.join(__dirname, 'lib', `url_client${getLibExtension()}`);
+const lib = koffi.load(libPath);
+
+const sendHttpRequest = lib.func('char* send_http_request(const char* url)');
+
+const url = 'https://srv.moonshot.money/categories?limit=10';
+
+app.get('/', function (req, res) {
+  try {
+    const result = sendHttpRequest(url);
+    res.json({ message: result });
+  } catch (error) {
+    console.error('Error calling send_http_request:', error);
+    res.status(500).json({ error: 'Failed to process request' });
+  }
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
